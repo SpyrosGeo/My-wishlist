eaa42bd6bfcd64471ee031941d492c412a52d2a7
SpyrosGeo <xarodasimba@gmail.com>
2025-02-02 20:51:19 +0200

    removed autocancellation for now and started css styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import './App.css'
 import Login from './components/Login/Login'
 import pb from './utils'
 import { logout } from './services/auth'
-import Wishlist from './components/Wishlist'
+import Wishlist from './components/Wishlist/Wishlist'
 function App() {
+  pb.autoCancellation(false);
   const [isLoggedIn, setIsLoggedIn] = useState(pb.authStore.isValid)
   const handleLogOut = () => {
 
@@ -14,7 +15,7 @@ function App() {
 
   return (
     <>
-  {isLoggedIn ? (<div>
+  {isLoggedIn ? (<div className="app__container">
     <button onClick={handleLogOut}>Log Out</button>
     <Wishlist/>
     </div>):
