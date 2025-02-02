8cc48ec85ed614d1d7488251d9d97485f6087be0
SpyrosGeo <xarodasimba@gmail.com>
2025-02-02 20:51:35 +0200

    removed autocancellation for now and started css styling

---
+++ src/components/Wishlist/Wishlist.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react"
+import { WishlistItem } from "../../interfaces";
+import WishlistItemComponent from "./WishlistItem";
+import { getWishlist } from "../../services/wishlist";
+
+function Wishlist() {
+  const [wishlist, setWishlist] = useState<WishlistItem[]>([]);
+  const [ error, setError ] = useState<string|null>("");
+  useEffect(() => {
+    const fetchWishlist = async () => {
+        try {
+           const items = await getWishlist();
+           if(!items?.length)return;
+           setWishlist(items);
+
+
+        } catch (error) {
+            setError("Error fetching wishlist");
+            
+        }
+    }
+    fetchWishlist();
+   }, [])
+  return (
+    <div>
+      <h2>My Wishlist</h2>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {wishlist.map((item) => (
+        <WishlistItemComponent key={item.id} item={item} />
+      ))}
+    </div>
+  )
+}
+
+export default Wishlist
---
+++ src/components/Wishlist/WishlistItem.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { WishlistItem } from "../../interfaces"
+import { updateWishlistItem } from "../../services/wishlist";
+import "./WishList.css";
+interface WishlistItemProps {
+    item:WishlistItem;
+}
+const  WishlistItemComponent:React.FC<WishlistItemProps> =({item})=> {
+  const [error, setError] = useState<string|null>("");
+  const handleTogglePurchased = async () => {
+    setError(null);
+    try {
+       await updateWishlistItem(item.id, { isPurchased: !item.isPurchased }); 
+    } catch (error) {
+       setError("Error updating item"); 
+    }
+  }
+
+  return (
+  <div className="wishlist-item__container">
+          <h3>{item.name}</h3>
+      <p>
+        <a href={item.url} target="_blank" rel="noopener noreferrer">
+          View Item
+        </a>
+      </p>
+      <p>Quantity: {item.quantity}</p>
+      <p>Status: {item.isPurchased ? 'Purchased' : 'Not Purchased'}</p>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+      <button onClick={handleTogglePurchased}>
+        Mark as {item.isPurchased ? 'Not Purchased' : 'Purchased'}
+      </button>
+  </div>
+  )
+}
+
+export default WishlistItemComponent
