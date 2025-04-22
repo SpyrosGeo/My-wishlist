afb9371a38eafab09ec94ca0cbaf59cf1c3ea2a8
SpyrosGeo <xarodasimba@gmail.com>
2025-04-22 21:39:46 +0300

    resetting error when needed

--- src/components/Wishlist.tsx
+++ src/components/Wishlist.tsx
@@ -1,35 +1,33 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 import { WishlistItem } from "../interfaces";
 import WishlistItemComponent from "./WishlistItem";
 import { getWishlist } from "../services/wishlist";
 
 function Wishlist() {
   const [wishlist, setWishlist] = useState<WishlistItem[]>([]);
-  const [ error, setError ] = useState<string|null>("");
+  const [error, setError] = useState<string | null>("");
   useEffect(() => {
     const fetchWishlist = async () => {
-        try {
-           const items = await getWishlist();
-           if(!items?.length)return;
-           setWishlist(items);
-
-
-        } catch (error) {
-            setError("Error fetching wishlist");
-            
-        }
-    }
+      try {
+        const items = await getWishlist();
+        if (!items?.length) return;
+        setError("");
+        setWishlist(items);
+      } catch (error) {
+        setError("Error fetching wishlist");
+      }
+    };
     fetchWishlist();
-   }, [])
+  }, []);
   return (
     <div>
       <h2>My Wishlist</h2>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
       {wishlist.map((item) => (
         <WishlistItemComponent key={item.id} item={item} />
       ))}
     </div>
-  )
+  );
 }
 
-export default Wishlist
+export default Wishlist;
